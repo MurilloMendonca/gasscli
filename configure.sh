89c6f629a23e8a82f4f9a2504d9ea9229c3a58bd
MurilloMendonca <murillo.ventura1711@unifei.edu.br>
2023-03-09 21:01:48 -0300

    Updated configure.sh to copy  config to ~/.gasscli

--- configure.sh
+++ configure.sh
@@ -3,4 +3,6 @@
 # Create the cache folder if it does not exist
 if [ ! -d "$HOME/.gasscli" ]; then
     mkdir "$HOME/.gasscli"
+    mkdir "$HOME/.gasscli/cache"
+    cp config.json "$HOME/.gasscli/config.json"
 fi
